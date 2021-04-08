d1e7a12cdd42e6ffb34873dd383be79f2567422b
Lisa Fellinger <fellingerlisa@hotmail.com>
2021-04-08 12:10:13 +0200

    seeds completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,55 +9,54 @@
 require "open-uri"
 puts 'Start seeding .... 😬'
 
-# User.destroy_all
-# Instrument.destroy_all
-# Booking.destroy_all
+Instrument.destroy_all
+User.destroy_all
+Booking.destroy_all
 
-# #User Piano Owner
-# chloe = User.new(
-#       first_name: 'Chloé',
-#       last_name: 'Durand',
-#       email: 'chloe@gmail.com',
-#       birthday: '12/11/1989',
-#       password: 'password',
-#     )
-# chloe.photo.attach(
-#             io: URI.open('https://cdn.psychologytoday.com/sites/default/files/field_blog_entry_images/2021-01/levi-williams-to5wnmst6qq-unsplash.jpg'),
-#             filename: 'chloe.jpg',
-#             content_type: 'image/jpg')
-# chloe.save!
+#User Piano Owner
+chloe = User.new(
+      first_name: 'Chloé',
+      last_name: 'Durand',
+      email: 'chloe@gmail.com',
+      birthday: '12/11/1989',
+      password: 'password',
+    )
+chloe.photo.attach(
+            io: URI.open('https://cdn.psychologytoday.com/sites/default/files/field_blog_entry_images/2021-01/levi-williams-to5wnmst6qq-unsplash.jpg'),
+            filename: 'chloe.jpg',
+            content_type: 'image/jpg')
+chloe.save!
 
-# nam = User.new(
-#       first_name: 'Nam',
-#       last_name: 'Nguyen',
-#       email: 'nam@gmail.com',
-#       birthday: '12/11/1972',
-#       password: 'password',
-#     )
-# nam.photo.attach(
-#             io: URI.open('https://cdn.xl.thumbs.canstockphoto.com/asian-playing-piano-a-shot-of-an-asian-man-playing-piano-stock-image_csp1567046.jpg'),
-#             filename: 'nam.jpg',
-#             content_type: 'nam/jpg')
-# nam.save!
+nam = User.new(
+      first_name: 'Nam',
+      last_name: 'Nguyen',
+      email: 'nam@gmail.com',
+      birthday: '12/11/1972',
+      password: 'password',
+    )
+nam.photo.attach(
+            io: URI.open('https://cdn.xl.thumbs.canstockphoto.com/asian-playing-piano-a-shot-of-an-asian-man-playing-piano-stock-image_csp1567046.jpg'),
+            filename: 'nam.jpg',
+            content_type: 'nam/jpg')
+nam.save!
 
-# #User Practise Seeker
-# lucy = User.new(
-#       first_name: 'Lucy',
-#       last_name: 'Smith',
-#       email: 'lucy@gmail.com',
-#       birthday: '12/11/1995',
-#       password: 'password',
-#     )
-# lucy.photo.attach(
-#             io: URI.open('https://static.roland.com/promos/starting_piano/faq_affordable_piano.jpg'),
-#             filename: 'lucy.jpg',
-#             content_type: 'image/jpg')
-# lucy.save!
+#User Practise Seeker
+lucy = User.new(
+      first_name: 'Lucy',
+      last_name: 'Smith',
+      email: 'lucy@gmail.com',
+      birthday: '12/11/1995',
+      password: 'password',
+    )
+lucy.photo.attach(
+            io: URI.open('https://static.roland.com/promos/starting_piano/faq_affordable_piano.jpg'),
+            filename: 'lucy.jpg',
+            content_type: 'image/jpg')
+lucy.save!
 
-# puts 'Users seed done! 💪'
+puts 'Users seed done! 💪'
 
 # Generate Cancellation Policy
-# array of all cancellation policies
 
 cancellation_policies = {
     '2 hours before practise' => 2,
@@ -74,26 +73,11 @@ cancellation_policies = {
 cancellation_policies.each do |policy, hour|
   CancellationPolicy.create!(
      name: policy,
-     hour: hour
+     hours: hour
     )
   end
 
-#policy = [2 hours, 3 hours, etc.] loop it and create all of it.
-
-# old:
-# cancellation_policy1 = CancellationPolicy.new(
-#                         instrument: acoustic_piano,
-#                         name: '2 hours before practise',
-#                         hours: 2,
-#     )
-# cancellation_policy1.save!
-
-# cancellation_policy2 = CancellationPolicy.new(
-#                         instrument: digital_piano,
-#                         name: '2 days before practise',
-#                         hours: 48,
-#     )
-# cancellation_policy2.save!
+  puts 'CancellationPolicy seed done! 💪'
 
 # Generate Instrument Instances
 acoustic_piano = Instrument.new(
@@ -105,8 +89,7 @@ acoustic_piano = Instrument.new(
                  longitude: 2.355338,
                  price: 20,
                  user: chloe,
-                 cancellation_policy: cancellation_policies.key(2),
-                 # add cancellation policy
+                 cancellation_policy: cancellation_policies[0],
   )
 acoustic_piano.photo.attach(
             io: URI.open('https://i.pinimg.com/originals/01/bc/4d/01bc4d9bb870b82465bbb1e7d839bbc3.jpg'),
@@ -114,37 +97,32 @@ acoustic_piano.photo.attach(
             content_type: 'image/jpg')
 acoustic_piano.save!
 
-# digital_piano = Instrument.new(
-#                  title: 'Digital Piano Paris 10e - Calme',
-#                  subtitle: 'Enjoy a good piano practise in a very quiet area',
-#                  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-#                  location: '80 Boulevard de Magenta, Paris, France',
-#                  latitude: 48.87617675877308,
-#                  longitude: 2.3566307396897184,
-#                  price: 15,
-#                  user: nam,
-#   )
-# digital_piano.photo.attach(
-#             io: URI.open('http://www.vend-appartement-paris.fr/wp-content/uploads/2018/08/hautpoul-s%C3%A9jour-2.jpg'),
-#             filename: 'digital_piano.jpg',
-#             content_type: 'image/jpg')
-# digital_piano.save!
+digital_piano = Instrument.new(
+                 title: 'Digital Piano Paris 10e - Calme',
+                 subtitle: 'Enjoy a good piano practise in a very quiet area',
+                 description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                 location: '80 Boulevard de Magenta, Paris, France',
+                 latitude: 48.87617675877308,
+                 longitude: 2.3566307396897184,
+                 price: 15,
+                 user: nam,
+  )
+digital_piano.photo.attach(
+            io: URI.open('http://www.vend-appartement-paris.fr/wp-content/uploads/2018/08/hautpoul-s%C3%A9jour-2.jpg'),
+            filename: 'digital_piano.jpg',
+            content_type: 'image/jpg')
+digital_piano.save!
 
-# puts 'Instrument seeds done! 💪'
-
-
-
-# puts 'CancellationPolicy seeds done! '
+puts 'Instrument seeds done! 💪'
 
 # Generate Booking Instances
-
-# booking1 = Booking.new(
-#             instrument: 2,
-#             user: 3,
-#             status: 'accepted',
-#             from: '13:26/2/7/2022',
-#             to: '14:26/2/7/2022',
-#   )
-# puts 'Booking seeds done!'
+booking1 = Booking.new(
+            instrument: digital_piano,
+            user: lucy,
+            status: 'accepted',
+            from: '13:26/2/7/2022',
+            to: '14:26/2/7/2022',
+  )
+puts 'Booking seeds done! 💪'
 puts 'Seed comlpeted! 🌱'
 
