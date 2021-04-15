530ebac19ebae2a103238522876dae8e941edab8
Lisa Fellinger <fellingerlisa@hotmail.com>
2021-04-15 20:56:35 +0200

    footer style & profile avatar style adjustments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,3 +119,12 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
+
+
+
+
+
+
+
+
+
