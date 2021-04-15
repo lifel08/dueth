af23eabb79e86e739d93b6dee915d77c55e9362f
Lisa Fellinger <fellingerlisa@hotmail.com>
2021-04-15 19:23:06 +0200

    added cloudname

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,5 +1,5 @@
 Cloudinary.config do |config|
-  config.cloud_name = 'sample'
+  config.cloud_name = 'dc446luao'
   config.api_key = '936841794962681'
   config.api_secret = 'ZFmPtBq2gA7TrH5h5ddJpeu2sA0'
   config.secure = true
