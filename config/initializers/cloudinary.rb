3c4656444bdfba02e27923d0fc4a2305637a9a4f
Lisa Fellinger <fellingerlisa@hotmail.com>
2021-04-15 19:20:20 +0200

    cloudinary initializers added

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'sample'
+  config.api_key = '936841794962681'
+  config.api_secret = 'ZFmPtBq2gA7TrH5h5ddJpeu2sA0'
+  config.secure = true
+  config.cdn_subdomain = true
+end
