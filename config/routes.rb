5ad2141402da2881a08e92351c3dba10cec55e85
Lisa Fellinger <fellingerlisa@hotmail.com>
2021-04-15 19:00:56 +0200

    redirect heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  get 'profile', to: 'pages#profile'
-  devise_for :users
+    root to: 'pages#home'
+    get 'profile', to: 'pages#profile'
+    devise_for :users
 
-  resources :instruments do
-    get ':title/:location/' => :search, on: :collection, as: :search
-    resources :bookings, only: [:new, :create, :update, :destroy]
-  end
-
-  resources :users do
-    resources :reviews, only: [:create, :edit, :destroy]
-  end
-
-  resources :bookings, only: :destroy do
-    member do
-      patch :accept
-      patch :decline
+    resources :instruments do
+      get ':title/:location/' => :search, on: :collection, as: :search
+      resources :bookings, only: [:new, :create, :update, :destroy]
     end
-    resources :reviews, only: [:create, :edit, :destroy]
+
+    resources :users do
+      resources :reviews, only: [:create, :edit, :destroy]
+    end
+
+    resources :bookings, only: :destroy do
+      member do
+        patch :accept
+        patch :decline
+      end
+      resources :reviews, only: [:create, :edit, :destroy]
+    end
+    resources :reviews, only: :destroy
   end
-  resources :reviews, only: :destroy
-end
