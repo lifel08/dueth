ac8d68854644e2da6fb480d8c30fe9d89ccb77f8
Lisa Fellinger <fellingerlisa@hotmail.com>
2021-05-01 10:23:30 +0200

    redirect empty search to hp

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,26 @@
 class BookingsController < ApplicationController
   def create
+    @booking = Booking.new
+    @booking.instrument = Instrument.find(params[:instrument_id])
+    @booking.user = current_user
+    @booking.status = false
+    redirect_to instrument_path(@booking.instrument), notice: "Pending approval of Instrument Owner #{@booking.instrument.provider_id.first_name}" if @booking.save
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
   end
 
   def accept
+    @booking = Booking.find(params[:id])
+    @booking.update(status: true)
+    redirect_to instrument_path, notice: "#{@booking.user.first_name}' s booking <strong>confirmed!</strong>"
   end
 
   def decline
+    @booking = Booking.find(params[:id])
+    @booking.update(status: false)
+    redirect_to instrument_path
   end
 end
