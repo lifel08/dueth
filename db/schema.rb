7242333f3176b9542eaba6323e797f8dadf5b77c
Lisa Fellinger <fellingerlisa@hotmail.com>
2022-01-11 22:32:12 +0100

    style public profile page & big instrument cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_01_05_180351) do
 
   # These are extensions that must be enabled in order to support this database
