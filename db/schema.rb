6e52f5394293da36a93b925c0e624b23b24050ff
Ferhan Nasim <ferhannasim@gmail.com>
2022-01-19 23:15:08 +0500

    added migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_05_180351) do
+ActiveRecord::Schema.define(version: 2022_01_19_152054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -139,7 +139,8 @@ ActiveRecord::Schema.define(version: 2022_01_05_180351) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "instrument_id"
+    t.bigint "instrument_id", null: false
+    t.bigint "instrument"
     t.index ["instrument_id"], name: "index_reviews_on_instrument_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
@@ -150,10 +151,6 @@ ActiveRecord::Schema.define(version: 2022_01_05_180351) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
@@ -161,6 +158,10 @@ ActiveRecord::Schema.define(version: 2022_01_05_180351) do
     t.datetime "birthday"
     t.string "language", default: "en"
     t.string "description"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["language"], name: "index_users_on_language"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
@@ -171,4 +172,5 @@ ActiveRecord::Schema.define(version: 2022_01_05_180351) do
   add_foreign_key "bookings", "disponibilities"
   add_foreign_key "instrument_features", "instruments"
   add_foreign_key "instruments", "users"
+  add_foreign_key "reviews", "instruments"
 end
