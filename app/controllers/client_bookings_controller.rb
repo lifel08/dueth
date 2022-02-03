e52a698160d30869c2f90238a54aa9995159ac8e
Lisa Fellinger <fellingerlisa@hotmail.com>
2022-02-03 07:43:58 +0100

    styling mobile search page

--- app/controllers/client_bookings_controller.rb
+++ app/controllers/client_bookings_controller.rb
@@ -1,7 +1,8 @@
 class ClientBookingsController < ApplicationController
 
   def index
-    add_breadcrumb "Your Bookings", :user_client_bookings_path
+    add_breadcrumb "My Profile", :profile_path
+    add_breadcrumb "My Bookings", :user_client_bookings_path
     @bookings = current_user.bookings
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -20,16 +20,15 @@ class Review < ApplicationRecord
   belongs_to :instrument
   validates :user_id, uniqueness: true
   validates :content, :instrument, presence: true
-  # enum rating: ["Excellent - Amazing Instrument & awesome Owner", "Good - Nice to play Instrument and respectful Owner","Bad - Instrument below expectations and unfriendly Owner"]
   after_save :update_rating
 
 
   def update_rating
-    if content == 'Excellent - Amazing Instrument & awesome Owner'
+    if content == 'Excellent! Amazing Instrument & awesome Owner'
       rating = 3
-    elsif content == 'Good - Nice to play Instrument and respectful Owner'
+    elsif content == 'Good! Nice to play Instrument and respectful Owner'
       rating = 2
-    else content == 'Bad - Below expectations Instrument and unfriendly Owner'
+    else content == 'Bad. Below expectations Instrument and unfriendly Owner'
       rating = 1
     end
     update_column(:rating, rating)
