812cc1e511e6b61b92f17d7763371a9945bd1fd5
Iman Fatima <ferhannasim@gmail.com>
2022-10-20 17:35:43 +0500

    my own booking fixes

--- app/controllers/client_bookings_controller.rb
+++ app/controllers/client_bookings_controller.rb
@@ -4,6 +4,6 @@ class ClientBookingsController < ApplicationController
     add_breadcrumb "My Profile", :profile_path
     add_breadcrumb "My Bookings", :user_client_bookings_path
     @instrument_availabilities = InstrumentAvailability.joins(instrument: [:user]).where(users: {id: current_user})
-    @instrument_booking = InstrumentAvailability.where(user_id: current_user.id)
+    @instrument_booking = Booking.where(receiver_id: current_user.id)
   end
 end
