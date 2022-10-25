acf40b5bcff71939248c8b81c96f7e8cdb5db88e
Iman Fatima <ferhannasim@gmail.com>
2022-10-25 16:39:49 +0500

    remove the extra fileds from popup and disable the location fields

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Dueth
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    # to read environment variables 
+    # to read environment variables
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
       YAML.load(File.open(env_file)).each do |key, value|
